8c309efc61ebdf70a0624929fac5ba1209c3f8c2
Balint <47783658+Roharx@users.noreply.github.com>
2024-02-03 14:04:00 +0100

    init

---
+++ Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace DefaultNamespace;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Substract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public double Divide(int a, int b)
+    {
+        return b != 0 ? a / b : 0;
+    }
+}
---
+++ CalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using DefaultNamespace;
+using NUnit.Framework;
+
+namespace DroneTestBalint;
+
+public class CalculatorTests(Calculator calculator)
+{
+    [SetUp]
+    public void Setup()
+    {
+        calculator = new Calculator();
+    }
+
+    [Test]
+    public void DivideByZero()
+    {
+        Assert.That(calculator.Divide(5, 0), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void NormalDivide()
+    {
+        Assert.That(calculator.Divide(6, 2), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void MultiplicationWithZero()
+    {
+        Assert.That(calculator.Multiply(5, 0), Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void Multiplication()
+    {
+        Assert.That(calculator.Multiply(5, 2), Is.EqualTo(10));
+    }
+
+    [Test]
+    public void AdditionWithNegative()
+    {
+        Assert.That(calculator.Add(5, -2), Is.EqualTo(3));
+    }
+    [Test]
+    public void Addition()
+    {
+        Assert.That(calculator.Add(5, 2), Is.EqualTo(7));
+    }
+    [Test]
+    public void SubtractWithNegative()
+    {
+        Assert.That(calculator.Add(5, -2), Is.EqualTo(7));
+    }
+    [Test]
+    public void Subtract()
+    {
+        Assert.That(calculator.Add(5, 2), Is.EqualTo(3));
+    }
+}
